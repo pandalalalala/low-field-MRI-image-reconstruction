a334556333914d444f67b93c5432d71340a2397a
BioMeEM <BioMeEM@10.21.121.250>
2018-06-26 15:46:45 +0800

    added ssim and psnr

--- add_noise.m
+++ add_noise.m
@@ -1,9 +1,9 @@
 % add noise onto signal vector
-function Sign_n = add_noise(Sign, Sign_time)
+function Sign_n = add_noise(Sign, Sign_time, Sample_N, N_angle, Noise_level)
 %% = constants =
-Sample_N=26;% Sample Number
-N_angle=50;% Rotating angle
-Noise_level=-70; % noise level in time domain (dB)
+% Sample_N=26;% Sample Number
+% N_angle=50;% Rotating angle
+% Noise_level=-40; % noise level in time domain (dB)
 
 V_noise_wgn_t=(10^(Noise_level/20)).*max(max(abs(Sign_time(1,:,:))))*wgn(Sample_N*8*N_angle,1,1,'real')';
 % wg_noise = wgn(Sample_N*COIL_N*N_angle,1,1,'real')';
--- main_image_boundary.m
+++ main_image_boundary.m
@@ -5,50 +5,61 @@ close all;
 %% get signals from simulation
 config
 images = import_images_june_2018(image_path, imformat, nlimit, ifresize, numrows, numcols);
-Obj_model = double(images{1});
+Obj_model = double(images{2});
 %     E_M = enc_gen(X,Y, B0_complete, dt, Sample_N, N_angle, coil_total,...
 %     Elev, r, Segment, I0, phi0, Pc, Azi, CurrentDir); % the encoding
 %     matrix is problemetic on 21st June.
-Obj_model = normalize(Obj_model);
 [Sign, Sign_time] = sig_gen_simul(Obj_model,E_M, dX, dY, dZ, gamma, Plank_h, T, k, N_per, N_angle, coil_total, Sample_N);
 %% image frame
 pic_size = sqrt(size(E_M,2)); % assume the reconstructed image to be squre
 [X_mri, Y_mri] = meshgrid(-pic_size/2+.5:pic_size/2-.5,-pic_size/2+.5:pic_size/2-.5);
 
 %% === Added noise ===
-Sign = add_noise(Sign, Sign_time);
+Sign = add_noise(Sign, Sign_time, Sample_N, N_angle, Noise_level)
 
 %% Least squares method 1
 [recon_image_LSM,error_LMS_nn] = LSMethod_gpu(E_M,Sign);
 picture_LSM = reshape(recon_image_LSM ,pic_size,pic_size);
-[peaksnrLSM,snrLSM] = psnr(picture_LSM,Obj_model);
+[peaksnrLSM,snrLSM] = psnr(picture_LSM,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_LSM,normalize(Obj_model,'range'));  
+fprintf('The LSM SSIM value is %0.4f.\n',ssimval);
+
 %% iteration Kaczmarz_su
 [recon_image_IT,error_IT_nn]=Kaczmarz_su(E_M,Sign,1,10); % lambda = 1, max iteration is 10
 picture_IT = reshape(recon_image_IT ,pic_size,pic_size);
-[peaksnrIT,snrIT] = psnr(picture_IT,Obj_model);
+[peaksnrIT,snrIT] = psnr(picture_IT,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_IT,normalize(Obj_model,'range'));  
+fprintf('The picture_IT SSIM value is %0.4f.\n',ssimval);
 
 %% TSVD
 [recon_image_TSVD,error_TSVD_nn] = TSVD(E_M,Sign);
 picture_TSVD = reshape(recon_image_TSVD ,pic_size,pic_size);
-[peaksnrTSVD,snrTSVD] = psnr(picture_TSVD,Obj_model);
-
+[peaksnrTSVD,snrTSVD] = psnr(picture_TSVD,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_TSVD,normalize(Obj_model,'range'));  
+fprintf('The picture_TSVD SSIM value is %0.4f.\n',ssimval);
 
 
 
 %% Least squares method boundary
 [recon_image_LSM,error_LMS_nn] = LSMethod_boundary(E_M,Sign);
 picture_LSM2 = reshape(recon_image_LSM ,pic_size,pic_size);
-[peaksnrLSM2,snrLSM2] = psnr(picture_LSM2,Obj_model);
+[peaksnrLSM2,snrLSM2] = psnr(picture_LSM2,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_LSM2,normalize(Obj_model,'range'));  
+fprintf('The picture_LSM2 SSIM value is %0.4f.\n',ssimval);
 
 %% iteration Kaczmarz_su boundary
 [recon_image_IT,error_IT_nn]=Kaczmarz_su_boundary(E_M,Sign); % lambda = 1, max iteration is 10
 picture_IT2 = reshape(recon_image_IT ,pic_size,pic_size);
-[peaksnrIT2,snrIT2] = psnr(picture_IT2,Obj_model);
+[peaksnrIT2,snrIT2] = psnr(picture_IT2,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_IT2,normalize(Obj_model,'range'));  
+fprintf('The picture_IT2 SSIM value is %0.4f.\n',ssimval);
 
 %% TSVD
 [recon_image_TSVD,error_TSVD_nn] = TSVD_boundary(E_M,Sign);
 picture_TSVD2 = reshape(recon_image_TSVD ,pic_size,pic_size);
-[peaksnrTSVD2,snrTSVD2] = psnr(picture_TSVD2,Obj_model);
+[peaksnrTSVD2,snrTSVD2] = psnr(picture_TSVD2,normalize(Obj_model,'range'));
+[ssimval, ssimmap] = ssim(picture_TSVD2,normalize(Obj_model,'range'));  
+fprintf('The picture_TSVD2 SSIM value is %0.4f.\n',ssimval);
 
 %% figures
 figure
