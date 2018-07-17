24e965c6fab164c4944c35b05051ff267f63d680
Richard <pandalalalala@gmail.com>
2018-07-17 14:49:53 +0800

    re-calculation

--- image_regen.m
+++ image_regen.m
@@ -1,7 +1,8 @@
-function [recon_image,error_LMS_nn] = image_regen(new_E_M,new_Signal,ssimmap,recon_image)
-[pre_recon_image,error_LMS_nn] = LSMethod_gpu(new_E_M,new_Signal);
+function [recon_image,error_LMS_nn] = image_regen(new_E_M,new_signal,ssimmap,recon_image)
+[pre_recon_image,error_LMS_nn] = TSVD(new_E_M,new_signal);
 ssimarray = reshape(ssimmap, 1,numel(ssimmap));
-[~,hole_col,~] = find(ssimarray < 0.9);
+threshold = 0.9;
+[~,hole_col,~] = find(ssimarray < threshold);
 disp(size(hole_col))
 for i = 1:length(pre_recon_image)
     recon_image(hole_col(i)) = pre_recon_image(i);
--- signal_regen.m
+++ signal_regen.m
@@ -1,11 +1,12 @@
-function [new_E, new_signal,picture] = signal_regen(E,signal, ssimmap, picture)
+function [new_E_M, new_signal,picture] = signal_regen(E_M,signal, ssimmap, picture)
 % [row,col,~] = find(ssimmap <= 0.9);
 % picture(row,col) = 0; these two lines are actually quite good!
-picture(ssimmap(:,:) <0.9) = 0;
+threshold = 0.9;
+picture(ssimmap(:,:) <threshold) = 0;
 m = reshape(picture, numel(picture),1);
-new_signal = signal - E * m;
+new_signal = signal - E_M * m;
 ssimarray = reshape(ssimmap, 1,numel(ssimmap));
-new_E = E;
-[~,col,~] = find(ssimarray >= 0.9);
-new_E(:,col) = [];
+new_E_M = E_M;
+[~,col,~] = find(ssimarray >= threshold);
+new_E_M(:,col) = [];
 end
