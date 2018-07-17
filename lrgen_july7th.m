1eb441e42b982c2e43fafd99c235a6a4ef5127bb
Richard <pandalalalala@gmail.com>
2018-07-17 20:50:21 +0800

    iteration to boundary map

--- lrgen_july7th.m
+++ lrgen_july7th.m
@@ -8,9 +8,18 @@ images = import_images_june_2018(image_path, imformat, nlimit, ifresize, numrows
 %%
 % E_M2 = getE_M2(E_M); % based on svd method
 %%
-for n_image = 1097:length(images)
-%     Obj_model = double(images{n_image});
-%     Obj_model = Obj_model(:,:,1);
+for n_image = 1:length(images)
+    Obj_model = (images{n_image});
+    Obj_model = Obj_model(:,:,1);
+%     head_area = head_polygon(Obj_model, 0.55);
+    [row,col,~] = find(Obj_model>=0.3*max(max(Obj_model)));
+    k = boundary(col,row,0.1);
+    [X,Y]=meshgrid(1:size(Obj_model,1),1:size(Obj_model,2));
+    head_area = inpolygon(X,Y,col(k),row(k));
+%     imwrite(images{n_image},[datapath, '\testbrain512\trainA\', int2str(n_image), '_A.jpg'])
+    head_area3 = cat(3,head_area,head_area,head_area);
+    imwrite(head_area3,[datapath, '\testbrain512\trainB\', int2str(n_image), '_B.jpg'])
+    %     Obj_model(Obj_model(:,:) <threshold) = 0; 
 %     [Sign, Sign_time] = sig_gen_simul(Obj_model,E_M, dX, dY, dZ, gamma, Plank_h, T, k, N_per, N_angle, coil_total, Sample_N);
 % % image frame
 %     pic_size = sqrt(size(E_M,2)); % assume the reconstructed image to be squre
@@ -25,6 +34,9 @@ for n_image = 1097:length(images)
 %     picture_TSVD = cat(3, picture_TSVD,picture_TSVD,picture_TSVD);
 %     picture_TSVD = imresize(picture_TSVD, [256,256]);
 %     imwrite(picture_TSVD, [datapath, '\testBTSVD\', int2str(n_image-1096), '_B.jpg'])
-    imwrite(images{n_image}, [datapath, '\testA\', int2str(n_image-1096), '_A.jpg'])
+%     imwrite(images{n_image}, [datapath, '\testA\', int2str(n_image-1096), '_A.jpg'])
 
 end
+disp('done!')
+subplot 121, imshow (head_area)
+subplot 122, imshow(Obj_model)
